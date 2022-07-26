61ae442098541c2acce59c37192085cbe3d8d010
Roman160784 <matveyenkaraman@gmail.com>
2022-07-26 18:50:30 +0300

    first App

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title='Push' onPress={() => {Alert.alert('TTT')}}/>
+
       <StatusBar style="auto" />
     </View>
   );
