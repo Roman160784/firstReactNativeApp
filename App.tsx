be728ae3cd0fcfec57359c0032a2f61dd6ace0ec
Roman160784 <matveyenkaraman@gmail.com>
2022-07-27 17:32:11 +0300

    show all array

--- App.tsx
+++ App.tsx
@@ -1,21 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { ActivityIndicator, Alert, Button, StyleSheet, Text, View, Image, TextInput, TouchableWithoutFeedback, TouchableOpacity, Keyboard } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { ActivityIndicator, Alert, Button, StyleSheet, Text, View, Image, TextInput, 
+  TouchableWithoutFeedback, TouchableOpacity, Keyboard, FlatList } from 'react-native';
 
-const arrayData = new Array (50)
+const arrayData = new Array (100)
 .fill(null)
 .map((_, index) => ({
   id: index + 1,
-  title: `Titlt  ${index + 1}`
+  title: `Title ${index + 1}`
 }))
 
 export default function App() {
   
-  const [value, setValue] = useState('')
+  const renderItem = useCallback(({item}) => (
+    <View>
+      <Text>{item.title}</Text>
+    </View>
+  ), [])
+  const keyExtractor = useCallback((item, index) => `${item.title}.${index}`, [])
+
 
   return (
     <View style={styles.container}>
       <Text>123123</Text>
+      <FlatList 
+      data={arrayData}
+      renderItem={renderItem}
+      keyExtractor={keyExtractor}
+      />
       <StatusBar style='auto'/>
     </View>
   );
